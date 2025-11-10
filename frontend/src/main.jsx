91fa986d6134022d29d88dd6a849b77169682316
Filip-Mihalj <fm55719@fer.hr>
2025-11-10 18:19:01 +0100

    dodan google maps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import "./index.css";
+import 'leaflet/dist/leaflet.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
