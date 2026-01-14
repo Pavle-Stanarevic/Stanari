0df05a9a1fdcbe94b483774836a34b0f9c511a02
Pavle-Stanarevic <pavle.stanarevic@fer.hr>
2026-01-14 16:49:36 +0100

    bug fix shop

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://backend:8080", // <<< OVO promijeni po nazivu servisa
         changeOrigin: true,
         secure: false,
       },
